b9940e80c9c922c1a4165d4fa0d5591725753777
Robert Devan Corona <devancorona@gmail.com>
2015-05-15 17:22:24 -0600

    Attempt to get webcam into BetterTower
    This uses the UVCCamera library
    (https://github.com/saki4510t/UVCCamera). This github also has sample
    code, USBCameraTest2 is the most simple/most close to what we need.
    
    Tried to merge the ideas in this sample code to tower as a fragment
    called video_feed_fragment, UI called from activity_flight.xml. Right
    now, just trying to get a sample video working as a fragment, but no
    dice…yet.

--- Android/src/org/droidplanner/android/fragments/VideoFeedFragment.java
+++ Android/src/org/droidplanner/android/fragments/VideoFeedFragment.java
@@ -1,10 +1,13 @@
 package org.droidplanner.android.fragments;
 
 import android.app.Fragment;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import org.droidplanner.android.R;
 
@@ -12,10 +15,13 @@ import org.droidplanner.android.R;
  * Created by converge-devan on 5/15/15.
  */
 public class VideoFeedFragment extends Fragment{
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_video_feed, container, false);
     }
+
+
 }
